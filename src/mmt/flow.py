e9b1e511e7f9c58042414e342b02435c45d696de
Tommaso Isolabella <tisolabella@ge.infn.it>
2023-04-26 11:11:42 +0200

    Working implementation of the preliminari alpha_FF fit

--- src/mmt/flow.py
+++ src/mmt/flow.py
@@ -60,7 +60,10 @@ if levo_booked:
     alpha_FF, alpha_WB = 1.0, 2.0
     # List for saving correlations
     alpha_BC_set = config['alpha BC values']
+    alpha_FF_set = config['alpha FF values']
+    alpha_WB_set = config['alpha WB values']
     for iteration_number in range(config['iterations']):
+        #------ Do the alpha_BC iteration -------------
         # List for the correlations
         R_2_alpha_BC = []
         for alpha_BC in alpha_BC_set:
@@ -96,16 +99,64 @@ if levo_booked:
             except Exception as e:
                 print(f'REGRESSION ERROR for ALPHA BC: {e}')
                 R_2_alpha_BC.append(0)
+        max_R2_index = R_2_alpha_BC.index(max(R_2_alpha_BC))
+        best_alpha_BC = alpha_BC_set[max_R2_index]
         # For checking
-        #print(alpha_BC_set, R_2_alpha_BC)
-        #plt.plot(alpha_BC_set, R_2_alpha_BC)
-        #plt.show()
+        print(alpha_BC_set, R_2_alpha_BC)
+        print("best alpha BC", best_alpha_BC, " for R2 ", R_2_alpha_BC[max_R2_index])
+        plt.plot(alpha_BC_set, R_2_alpha_BC)
+        plt.show()
+
+        #------ Do the alpha_FF iteration -------------
+        # List for the correlations
+        R_2_alpha_FF = []
+        for alpha_FF in alpha_FF_set:
+            # List to save the BC_WB(lambda_long) and levo
+            BC_WB_set, levo_set = [], []
+            # Fix the alpha BC value in a new function 
+            def typefit(x, A, B, alpha_BrC):
+                return doublefit(x, A, best_alpha_BC, B, alpha_BrC)
+            # Fix the alpha FF value in a new function 
+            def sourcefit(x, A_p, B_p):
+                return doublefit(x, A_p, alpha_FF, B_p, alpha_WB)
+            # Iterate over all the samples
+            for sample in data:
+                prop = sample.properties
+                # Do fit
+                try:
+                    typefitres = curve_fit(typefit, prop.wavelength,
+                            prop.abs, p0=(1e3, 1e10, 3),
+                            bounds=([1, 1, 1], [1e15, 1e15, 10]),
+                            sigma=prop.u_abs)
+                    sourcefitres = curve_fit(sourcefit, prop.wavelength,
+                            prop.abs, p0=(1e3, 1e10),
+                            bounds=([1, 1], [1e15, 1e15]),
+                            sigma=prop.u_abs)
+                    A = typefitres[0][0]
+                    B = typefitres[0][1]
+                    alpha_BrC = typefitres[0][2]
+                    A_p = sourcefitres[0][0]
+                    B_p = sourcefitres[0][1]
+                    # Apportion BC WB at the longest wavelength
+                    lambda_long =  max(prop.wavelength) 
+                    BC_WB = (A - A_p) * lambda_long ** ( - best_alpha_BC) 
+                    BC_WB_set.append(BC_WB)
+                except Exception as e:
+                    print(f'FIT ERROR for ALPHA_FF: {e}')
+                    BC_WB_set.append(0)
+                levo_set.append(prop.Levoglucosan)
+            # Calculate regression and append R^2
+            try:
+                regression_res = linregress(levo_set, y=BC_WB_set)
+                R_2_alpha_FF.append(regression_res.rvalue ** 2)
+            except Exception as e:
+                print(f'REGRESSION ERROR for ALPHA FF: {e}')
+                R_2_alpha_FF.append(0)
+        max_R2_index = R_2_alpha_FF.index(max(R_2_alpha_FF))
+        best_alpha_FF = alpha_FF_set[max_R2_index]
+        # For checking
+        print(alpha_FF_set, R_2_alpha_FF)
+        print("best alpha FF", best_alpha_FF, " for R2 ", R_2_alpha_FF[max_R2_index])
+        plt.plot(alpha_FF_set, R_2_alpha_FF)
+        plt.show()
         # It works so far!
-    
-
-
-
-
-
-
-
